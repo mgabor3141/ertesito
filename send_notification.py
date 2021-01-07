b38f53521b1daf89428811590731a73297cf8f12
Gábor Magyar <gabormagyar@vacuumlabs.com>
2021-01-07 12:27:46 +0100

    Change message order, remove first path entry and add version number

--- send_notification.py
+++ send_notification.py
@@ -17,7 +17,7 @@ def print_path(path):
     if len(path) == 0:
         return "?"
 
-    return " / ".join(path)
+    return " / ".join(path[1:])
 
 
 VOWELS = ['a', 'á', 'e', 'é', 'i', 'í', 'o', 'ó', 'ö', 'ő', 'u', 'ú', 'ü', 'ű']
@@ -27,19 +27,19 @@ def data_to_html(added, removed, name):
     html = f"<p>Kedves {name}!</p>"
     html += f"<p>Változás történt az ambuláns beosztásodban:</p>"
 
-    if len(added) > 0:
-        html += f"<h3>Új bejegyzés{'ek' if len(added) > 1 else ''}:</h3><p>"
-
-        for entry in added:
-            html += f"<u>{print_path(entry[0])}:</u> {entry[1]}<br />"
-
-        html += "</p>"
-
     if len(removed) > 0:
         html += f"<h3>Törölt bejegyzés{'ek' if len(removed) > 1 else ''}:</h3><p>"
 
         for entry in removed:
             html += f"<u>{print_path(entry[0])}:</u> <strike>{entry[1]}</strike> <br />"
+
+        html += "</p>"
+
+    if len(added) > 0:
+        html += f"<h3>Új bejegyzés{'ek' if len(added) > 1 else ''}:</h3><p>"
+
+        for entry in added:
+            html += f"<u>{print_path(entry[0])}:</u> {entry[1]}<br />"
 
         html += "</p>"
 
@@ -49,7 +49,8 @@ def data_to_html(added, removed, name):
 
     html += "<hr/><small>A táblázat óránként van ellenőrizve, a személyre szóló naptárad is ezzel egyszerre frissül.<br /> \
             Válaszlevélben jelezd, ha kérésed vagy kérdésed van, illetve ha nem szeretnél több ilyen értesítést kapni.<br/>\
-            Az adatok tájékoztató jellegűek, a helyességükért vagy teljességükért felelősséget senki nem vállal.</small>"
+            Az adatok tájékoztató jellegűek, a helyességükért vagy teljességükért felelősséget senki nem vállal.<br/>\
+            <small>Verzió: 2.1</small></small>"
 
     return html
 
@@ -73,11 +74,11 @@ def send_notifications(added, removed):
 
 if __name__ == "__main__":
     _added = [
-        (('Január Ambulancia', '2021.01.01.', 'péntek', 'I Műszak', 'Pretriázs'), 'Pretriázs: dr. Magyar Gábor')
+        (('2021 Ambulancia', 'Január Ambulancia', '2021.01.01.', 'péntek', 'I Műszak', 'Pretriázs'), 'Pretriázs: dr. Magyar Gábor')
     ]
 
     _removed = [
-        (('Január Ambulancia', '2021.01.01.', 'péntek', 'I Műszak', 'Pretriázs'), 'Pretriázs: dr. Svidro Eszter')
+        (('2021 Ambulancia', 'Január Ambulancia', '2021.01.01.', 'péntek', 'I Műszak', 'Pretriázs'), 'Pretriázs: dr. Svidro Eszter')
     ]
 
     print(send_notifications(_added, _removed))
